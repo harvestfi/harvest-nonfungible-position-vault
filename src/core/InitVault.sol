d051a704418dbb0ffd4d35b91dddef9d2bf074b2
ryanycw <ryan@ryanycw.dev>
2023-08-01 00:33:25 +0800

    build: init vault module

--- src/core/InitVault.sol
+++ src/core/InitVault.sol
@@ -3,17 +3,24 @@ pragma solidity 0.8.17;
 
 // External Packages
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 // Libraries
 import {AppStorage, LibAppStorage} from "../libraries/LibAppStorage.sol";
 import {LibPostionManager} from "../libraries/LibPostionManager.sol";
 
-contract InitVault is ERC20Upgradeable {
-    function initialize(uint256 _tokenId) public {
+contract InitVault is Initializable, ERC20Upgradeable {
+    function initialize(address _nftPositionManager, address _masterchef, uint256 _tokenId, string calldata _vaultName)
+        public
+        initializer
+    {
         AppStorage storage s = LibAppStorage.systemStorage();
+        s.tokenId = _tokenId;
+        s.nonFungibleTokenPositionManager = _nftPositionManager;
+        s.masterChef = _masterchef;
 
         // stake position
-        LibPostionManager.stake(_tokenId);
+        LibPostionManager.stake();
 
         // fetch info from position manager
         (address _token0, address _token1, uint24 _fee, int24 _tickLower, int24 _tickUpper, uint256 _initialLiquidity) =
@@ -26,12 +33,11 @@ contract InitVault is ERC20Upgradeable {
         s.tickLower = _tickLower;
         s.tickUpper = _tickUpper;
         s.initialLiquidity = _initialLiquidity;
-        s.tokenId = _tokenId;
 
         // initializing the vault token. By default, it has 18 decimals.
         __ERC20_init_unchained(
-            string(abi.encodePacked("fUniV3_", ERC20Upgradeable(_token0).symbol(), "_", ERC20Upgradeable(_token1).symbol())),
-            string(abi.encodePacked("fUniV3_", ERC20Upgradeable(_token0).symbol(), "_", ERC20Upgradeable(_token1).symbol()))
+            string(abi.encodePacked(_vaultName, ERC20Upgradeable(_token0).symbol(), "_", ERC20Upgradeable(_token1).symbol())),
+            string(abi.encodePacked(_vaultName, ERC20Upgradeable(_token0).symbol(), "_", ERC20Upgradeable(_token1).symbol()))
         );
 
         // mint the initial shares and send them to the specified recipient, as well as overflow of the tokens
