49903e1430651a3b2a9709cd70930d65947595e9
ryanycw <ryan@ryanycw.dev>
2023-08-04 17:21:26 +0800

    build: init module structure

--- src/submodules/JoinSubmodule.sol
+++ src/submodules/JoinSubmodule.sol
@@ -1,7 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+// Libraries
+import {AppStorage, LibAppStorage} from "../libraries/LibAppStorage.sol";
+import {LibPostionManager} from "../libraries/LibPostionManager.sol";
+
 // Helpers
 import {Modifiers} from "../core/Modifiers.sol";
 
-contract JoinSubmodule is Modifiers {}
+contract JoinSubmodule is Modifiers {
+    AppStorage internal s;
+
+    function createPosition() public {
+        LibPostionManager.mint();
+    }
+
+    function stakePosition() public {
+        LibPostionManager.stake();
+    }
+}
