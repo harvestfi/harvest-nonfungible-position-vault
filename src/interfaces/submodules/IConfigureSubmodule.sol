8167da679ebfc0b3e6cd1e179e4efba33e3c7cf1
ryanycw <ryan@ryanycw.dev>
2023-08-07 00:49:58 +0800

    build: add functions to interfaces

--- src/interfaces/submodules/IConfigureSubmodule.sol
+++ src/interfaces/submodules/IConfigureSubmodule.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.17;
 interface IConfigureSubmodule {
     function configureFees(address _feeRewardForwarder, uint256 _feeRatio, address _platformTarget, uint256 _platformRatio)
         external;
-
+    function configureExternalProtocol(address _nftPositionManager, address _masterchef) external;
+    function configurePool(address _token0, address _token1, uint24 _fee, string calldata _vaultName) external;
     function setVaultPause(bool _pause) external;
 }
