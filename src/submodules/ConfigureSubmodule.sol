a55de23d545ee2141bbd862b1f352cde64256a74
ryanycw <ryan@ryanycw.dev>
2023-08-07 00:50:58 +0800

    build: configuration to the vault

--- src/submodules/ConfigureSubmodule.sol
+++ src/submodules/ConfigureSubmodule.sol
@@ -28,6 +28,30 @@ contract ConfigureSubmodule is Modifiers, IConfigureSubmodule {
         emit LibEvents.FeeConfigurationUpdate(_feeRewardForwarder, _feeRatio, _platformTarget, _platformRatio);
     }
 
+    function configureExternalProtocol(address _nftPositionManager, address _masterchef)
+        external
+        override
+        onlyGovernanceOrController
+    {
+        s.nonFungibleTokenPositionManager = _nftPositionManager;
+        s.masterChef = _masterchef;
+
+        emit LibEvents.ExternalFarmingContractUpdate(_nftPositionManager, _masterchef);
+    }
+
+    function configurePool(address _token0, address _token1, uint24 _fee, string calldata _vaultName)
+        external
+        override
+        onlyGovernanceOrController
+    {
+        s.token0 = _token0;
+        s.token1 = _token1;
+        s.fee = _fee;
+        s.vaultName = _vaultName;
+
+        emit LibEvents.PoolConfigurationUpdate(_token0, _token1, _fee, _vaultName);
+    }
+
     function setVaultPause(bool _pause) public override onlyGovernanceOrController {
         s.vaultPause = _pause;
 
