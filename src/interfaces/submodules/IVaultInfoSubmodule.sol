6a76efa26eb74d052ab02e121924468291fbc9bf
ryanycw <ryan@ryanycw.dev>
2023-08-14 23:09:04 +0800

    build: add interface for state variables

--- src/interfaces/submodules/IVaultInfoSubmodule.sol
+++ src/interfaces/submodules/IVaultInfoSubmodule.sol
@@ -1,4 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-interface IVaultInfoSubmodule {}
+// Interfaces
+import {Position} from "../../libraries/LibAppStorage.sol";
+
+interface IVaultInfoSubmodule {
+    // System
+    function initialized() external view returns (uint8 _initialized);
+    function initializing() external view returns (bool _initializing);
+    function vaultPause() external view returns (bool _vaultPause);
+    function governance() external view returns (address _governance);
+    function controller() external view returns (address _controller);
+    // Fee Reward Shares
+    function feeRewardForwarder() external view returns (address _feeRewardForwarder);
+    function platformTarget() external view returns (address _platformTarget);
+    function feeRatio() external view returns (uint256 _feeRatio);
+    function platformRatio() external view returns (uint256 _platformRatio);
+    /// Simple two phase upgrade scheme
+    function upgradeScheduled(bytes32 _id) external view returns (uint256 _upgradeScheduled);
+    function upgradeExpiration() external view returns (uint256 _upgradeExpiration);
+    // Vault
+    function token0() external view returns (address _token0);
+    function token1() external view returns (address _token1);
+    function fee() external view returns (uint24 _fee);
+    // Token
+    function balances(address _account) external view returns (uint256 _balances);
+    function allowances(address _owner, address _spender) external view returns (uint256 _allowances);
+    function totalSupply() external view returns (uint256 _totalSupply);
+    function name() external view returns (string memory _name);
+    function symbol() external view returns (string memory _symbol);
+    // Position
+    function positionCount() external view returns (uint256 _count);
+    function latestTokenId() external view returns (uint256 _tokenId);
+    function positions(uint256 _tokenId) external view returns (Position memory _position);
+    function allPosition() external view returns (Position[] memory _positions);
+    // External
+    function nonFungibleTokenPositionManager() external view returns (address _nonFungibleTokenPositionManager);
+    function masterChef() external view returns (address _masterChef);
+}
--- src/submodules/VaultInfoSubmodule.sol
+++ src/submodules/VaultInfoSubmodule.sol
@@ -4,16 +4,120 @@ pragma solidity 0.8.17;
 // Interfaces
 import {IVaultInfoSubmodule} from "../interfaces/submodules/IVaultInfoSubmodule.sol";
 
+// Libraries
+import {Position} from "../libraries/LibAppStorage.sol";
+
 // Helpers
 import {Modifiers} from "../core/Modifiers.sol";
 
 contract VaultInfoSubmodule is Modifiers, IVaultInfoSubmodule {
-/**
- * @dev Returns the total liquidity stored in the position
- * Dev Note: need to turn on the solc optimizer otherwise the compiler
- * will throw stack too deep on this function
- */
-/*
+    function initialized() external view override returns (uint8 _initialized) {
+        return s.initialized;
+    }
+
+    function initializing() external view override returns (bool _initializing) {
+        return s.initializing;
+    }
+
+    function vaultPause() external view override returns (bool _vaultPause) {
+        return s.vaultPause;
+    }
+
+    function governance() external view override returns (address _governance) {
+        return s.governance;
+    }
+
+    function controller() external view override returns (address _controller) {
+        return s.controller;
+    }
+
+    function feeRewardForwarder() external view override returns (address _feeRewardForwarder) {
+        return s.feeRewardForwarder;
+    }
+
+    function platformTarget() external view override returns (address _platformTarget) {
+        return s.platformTarget;
+    }
+
+    function feeRatio() external view override returns (uint256 _feeRatio) {
+        return s.feeRatio;
+    }
+
+    function platformRatio() external view override returns (uint256 _platformRatio) {
+        return s.platformRatio;
+    }
+
+    function upgradeScheduled(bytes32 _id) external view override returns (uint256 _upgradeScheduled) {
+        return s.upgradeScheduled[_id];
+    }
+
+    function upgradeExpiration() external view override returns (uint256 _upgradeExpiration) {
+        return s.upgradeExpiration;
+    }
+
+    function token0() external view override returns (address _token0) {
+        return s.token0;
+    }
+
+    function token1() external view override returns (address _token1) {
+        return s.token1;
+    }
+
+    function fee() external view override returns (uint24 _fee) {
+        return s.fee;
+    }
+
+    function balances(address _account) external view override returns (uint256 _balances) {
+        return s.balances[_account];
+    }
+
+    function allowances(address _owner, address _spender) external view override returns (uint256 _allowances) {
+        return s.allowances[_owner][_spender];
+    }
+
+    function totalSupply() external view override returns (uint256 _totalSupply) {
+        return s.totalSupply;
+    }
+
+    function name() external view override returns (string memory _name) {
+        return s.name;
+    }
+
+    function symbol() external view override returns (string memory _symbol) {
+        return s.symbol;
+    }
+
+    function positionCount() external view override returns (uint256 _count) {
+        return s.positionCount;
+    }
+
+    function latestTokenId() external view override returns (uint256 _tokenId) {
+        return s.latestPositionId;
+    }
+
+    function positions(uint256 _tokenId) external view override returns (Position memory _position) {
+        return s.positions[_tokenId];
+    }
+
+    function allPosition() external view override returns (Position[] memory _positions) {
+        for (uint256 i; i < s.positionCount; i++) {
+            _positions[i] = s.positions[i];
+        }
+    }
+
+    function nonFungibleTokenPositionManager() external view override returns (address _nonFungibleTokenPositionManager) {
+        return s.nonFungibleTokenPositionManager;
+    }
+
+    function masterChef() external view override returns (address _masterChef) {
+        return s.masterChef;
+    }
+    /**
+     * @dev Returns the total liquidity stored in the position
+     * Dev Note: need to turn on the solc optimizer otherwise the compiler
+     * will throw stack too deep on this function
+     */
+    /*
     function underlyingBalanceWithInvestment() public view returns (uint256) {
         // note that the liquidity is not a token, so there is no local balance added
         (,,,,,,, uint128 liquidity,,,,) = INonfungiblePositionManager(_NFT_POSITION_MANAGER).positions(getStorage().posId());
@@ -21,10 +125,10 @@ contract VaultInfoSubmodule is Modifiers, IVaultInfoSubmodule {
     }
     */
 
-/**
- * @dev Returns the price per full share, scaled to 1e18
- */
-/*
+    /**
+     * @dev Returns the price per full share, scaled to 1e18
+     */
+    /*
     function getPricePerFullShare() public view returns (uint256) {
         return totalSupply() == 0 ? _UNDERLYING_UNIT : _UNDERLYING_UNIT.mul(underlyingBalanceWithInvestment()).div(totalSupply());
     }
@@ -33,33 +137,16 @@ contract VaultInfoSubmodule is Modifiers, IVaultInfoSubmodule {
     /**
      * @dev Convenience getter for the data contract.
      */
-/*
+    /*
     function getStorage() public view returns (IUniVaultStorageV1) {
         return IUniVaultStorageV1(getAddress(_DATA_CONTRACT_SLOT));
     }
     */
 
-/**
- * @dev Convenience getter for token0.
- */
-/*
-    function token0() public view returns (IERC20Upgradeable) {
-        return IERC20Upgradeable(getStorage().token0());
-    }
-
     /**
-     * @dev Convenience getter for token1.
+     * @dev Convenience getter for the current sqrtPriceX96 of the Uniswap pool.
      */
-/*
-    function token1() public view returns (IERC20Upgradeable) {
-        return IERC20Upgradeable(getStorage().token1());
-    }
-    */
-
-/**
- * @dev Convenience getter for the current sqrtPriceX96 of the Uniswap pool.
- */
-/*
+    /*
     function getSqrtPriceX96() public view returns (uint160) {
         address poolAddr =
             IUniswapV3Factory(_UNI_POOL_FACTORY).getPool(getStorage().token0(), getStorage().token1(), getStorage().fee());
@@ -68,17 +155,11 @@ contract VaultInfoSubmodule is Modifiers, IVaultInfoSubmodule {
     }
     */
 
-/*
+    /*
     function submoduleAddress(bytes32 submoduleHash) public view returns (address) {
         GlobalStorage storage globalStorage = getGlobalStorage();
         address currentSubmodule = globalStorage.submoduleAddress[submoduleHash];
         return currentSubmodule;
-    }
-    */
-
-/*
-    function vaultPaused() public view returns (bool) {
-        return getBoolean(_VAULT_PAUSE_SLOT);
     }
     */
 }
