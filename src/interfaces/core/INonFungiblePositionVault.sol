be2c28e92fd2435fd98db8d4533a14b1a2db0f42
ryanycw <ryan@ryanycw.dev>
2023-08-22 22:11:17 +0800

    refactor: move openzeppelin feature to submodules

--- src/interfaces/core/INonFungiblePositionVault.sol
+++ src/interfaces/core/INonFungiblePositionVault.sol
@@ -1,26 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-// External Packages
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
 // Submodules
 import {IConfigureSubmodule} from "../submodules/IConfigureSubmodule.sol";
-import {IExitSubmodule} from "../submodules/IExitSubmodule.sol";
-import {IFunctionInfoSubmodule} from "../submodules/IFunctionInfoSubmodule.sol";
+import {IUpgradeSubmodule} from "../submodules/IUpgradeSubmodule.sol";
 import {IGovernanceSubmodule} from "../submodules/IGovernanceSubmodule.sol";
+import {IFunctionInfoSubmodule} from "../submodules/IFunctionInfoSubmodule.sol";
+import {ITokenizedVaultSubmodule} from "../submodules/ITokenizedVaultSubmodule.sol";
 import {IInvestSubmodule} from "../submodules/IInvestSubmodule.sol";
 import {IJoinSubmodule} from "../submodules/IJoinSubmodule.sol";
-import {IUpgradeSubmodule} from "../submodules/IUpgradeSubmodule.sol";
+import {IExitSubmodule} from "../submodules/IExitSubmodule.sol";
 import {IVaultInfoSubmodule} from "../submodules/IVaultInfoSubmodule.sol";
 
 interface INonFungiblePositionVault is
-    IERC20Upgradeable,
     IConfigureSubmodule,
     IUpgradeSubmodule,
     IGovernanceSubmodule,
     IFunctionInfoSubmodule,
+    ITokenizedVaultSubmodule,
     IJoinSubmodule,
     IInvestSubmodule,
     IExitSubmodule,
