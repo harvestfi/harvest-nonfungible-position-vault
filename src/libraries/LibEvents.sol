7ca09d4b9ccd3ebe4ddad14834dcc55c46c7ec3d
ryanycw <ryan@ryanycw.dev>
2023-08-23 15:40:54 +0800

    build: add new event for invest module

--- src/libraries/LibEvents.sol
+++ src/libraries/LibEvents.sol
@@ -5,29 +5,29 @@ pragma solidity 0.8.17;
 import {LibDataTypes} from "./LibDataTypes.sol";
 
 library LibEvents {
+    // Ownership
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    // Upgrade
     event SubmoduleUpgrade(LibDataTypes.SubmoduleUpgrade[] _submoduleUpgrade, address _init, bytes _calldata);
-
     event CreateUpgrade(bytes32 id, address indexed who);
     event UpdateUpgradeExpiration(uint256 duration);
     event UpgradeCancelled(bytes32 id, address indexed who);
-
+    // Configuration
     event VaultPauseUpdate(bool paused);
     event FeeConfigurationUpdate(address feeRewardForwarder, uint256 feeRatio, address platformTarget, uint256 platformRatio);
     event ExternalFarmingContractUpdate(address indexed nftPositionManager, address indexed masterchef);
+    event InfrastructureUpdate(address indexed universalLiquidator, address indexed universalLiquidatorRegistry);
     event PoolConfigurationUpdate(address indexed token0, address indexed token1, uint24 fee, string name);
-
-    // Configuration
     event PositionAdd(uint256 positionId, uint256 tokenId, uint256 liquidity, int24 tickLower, int24 tickUpper);
     event PositionUpdate(uint256 positionId, uint256 tokenId, uint256 liquidity, int24 tickLower, int24 tickUpper);
-
     // Initializable
     event Initialized(uint8 version);
-
     // TokenizedVault
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
-
+    // Position Operations
+    event RewardLiquidationPaused(bool paused);
+    //
     event SwapFeeClaimed(uint256 token0Fee, uint256 token1Fee, uint256 timestamp);
     event Deposit(address indexed user, uint256 token0Amount, uint256 token1Amount);
     event Withdraw(address indexed user, uint256 token0Amount, uint256 token1Amount);
