38f9ce8da6e4fc4d96d151015078166678543cb4
ryanycw <ryan@ryanycw.dev>
2023-08-26 22:16:47 +0800

    build: add new struct for masterchef

--- src/libraries/LibDataTypes.sol
+++ src/libraries/LibDataTypes.sol
@@ -14,4 +14,28 @@ library LibDataTypes {
         SubmoduleUpgradeAction action;
         bytes4[] functionSelectors;
     }
+
+    struct IncreaseLiquidityParams {
+        uint256 tokenId;
+        uint256 amount0Desired;
+        uint256 amount1Desired;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        uint256 deadline;
+    }
+
+    struct DecreaseLiquidityParams {
+        uint256 tokenId;
+        uint128 liquidity;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        uint256 deadline;
+    }
+
+    struct CollectParams {
+        uint256 tokenId;
+        address recipient;
+        uint128 amount0Max;
+        uint128 amount1Max;
+    }
 }
