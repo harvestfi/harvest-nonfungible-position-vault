802d665d47fcd065533a62d9137063dcc0cc1644
ryanycw <ryan@ryanycw.dev>
2023-08-26 22:12:17 +0800

    build: add internal interfaces

---
+++ src/interfaces/utils/IController.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface IController {
+    // ========================= Events =========================
+
+    event QueueProfitSharingChange(uint256 profitSharingNumerator, uint256 validAtTimestamp);
+    event ConfirmProfitSharingChange(uint256 profitSharingNumerator);
+
+    event QueueStrategistFeeChange(uint256 strategistFeeNumerator, uint256 validAtTimestamp);
+    event ConfirmStrategistFeeChange(uint256 strategistFeeNumerator);
+
+    event QueuePlatformFeeChange(uint256 platformFeeNumerator, uint256 validAtTimestamp);
+    event ConfirmPlatformFeeChange(uint256 platformFeeNumerator);
+
+    event QueueNextImplementationDelay(uint256 implementationDelay, uint256 validAtTimestamp);
+    event ConfirmNextImplementationDelay(uint256 implementationDelay);
+
+    event AddedStakingContract(address indexed stakingContract);
+    event RemovedStakingContract(address indexed stakingContract);
+
+    event SharePriceChangeLog(
+        address indexed vault, address indexed strategy, uint256 oldSharePrice, uint256 newSharePrice, uint256 timestamp
+    );
+
+    // ==================== Functions ====================
+
+    /**
+     * An EOA can safely interact with the system no matter what. If you're using Metamask, you're using an EOA. Only
+     * smart contracts may be affected by this grey list. This contract will not be able to ban any EOA from the system
+     * even if an EOA is being added to the greyList, he/she will still be able to interact with the whole system as if
+     * nothing happened. Only smart contracts will be affected by being added to the greyList. This grey list is only
+     * used in VaultV3.sol, see the code there for reference
+     */
+    function greyList(address _target) external view returns (bool);
+
+    function addressWhiteList(address _target) external view returns (bool);
+
+    function codeWhiteList(address _target) external view returns (bool);
+
+    function addToWhitelist(address _target) external;
+
+    function addCodeToWhitelist(address _target) external;
+
+    function store() external view returns (address);
+
+    function governance() external view returns (address);
+
+    function doHardWork(address _vault) external;
+
+    function addHardWorker(address _worker) external;
+
+    function removeHardWorker(address _worker) external;
+
+    function salvage(address _token, uint256 amount) external;
+
+    function salvageStrategy(address _strategy, address _token, uint256 amount) external;
+
+    /**
+     * @return The targeted profit token to convert all-non-compounding rewards to. Defaults to WETH.
+     */
+    function targetToken() external view returns (address);
+
+    function setTargetToken(address _targetToken) external;
+
+    function profitSharingReceiver() external view returns (address);
+
+    function setProfitSharingReceiver(address _profitSharingReceiver) external;
+
+    function protocolFeeReceiver() external view returns (address);
+
+    function setProtocolFeeReceiver(address _protocolFeeReceiver) external;
+
+    function rewardForwarder() external view returns (address);
+
+    function setRewardForwarder(address _rewardForwarder) external;
+
+    function universalLiquidator() external view returns (address);
+
+    function setUniversalLiquidator(address _universalLiquidator) external;
+
+    function dolomiteYieldFarmingRouter() external view returns (address);
+
+    function setDolomiteYieldFarmingRouter(address _value) external;
+
+    function nextImplementationDelay() external view returns (uint256);
+
+    function profitSharingNumerator() external view returns (uint256);
+
+    function strategistFeeNumerator() external view returns (uint256);
+
+    function platformFeeNumerator() external view returns (uint256);
+
+    function feeDenominator() external view returns (uint256);
+
+    function setProfitSharingNumerator(uint256 _profitSharingNumerator) external;
+
+    function confirmSetProfitSharingNumerator() external;
+
+    function setStrategistFeeNumerator(uint256 _strategistFeeNumerator) external;
+
+    function confirmSetStrategistFeeNumerator() external;
+
+    function setPlatformFeeNumerator(uint256 _platformFeeNumerator) external;
+
+    function confirmSetPlatformFeeNumerator() external;
+
+    function setNextImplementationDelay(uint256 _nextImplementationDelay) external;
+
+    function confirmNextImplementationDelay() external;
+
+    function nextProfitSharingNumerator() external view returns (uint256);
+
+    function nextProfitSharingNumeratorTimestamp() external view returns (uint256);
+
+    function nextStrategistFeeNumerator() external view returns (uint256);
+
+    function nextStrategistFeeNumeratorTimestamp() external view returns (uint256);
+
+    function nextPlatformFeeNumerator() external view returns (uint256);
+
+    function nextPlatformFeeNumeratorTimestamp() external view returns (uint256);
+
+    function tempNextImplementationDelay() external view returns (uint256);
+
+    function tempNextImplementationDelayTimestamp() external view returns (uint256);
+}
