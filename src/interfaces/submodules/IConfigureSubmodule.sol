201e62a0321a7c4f4f0238ec3d3bac0c9c0ec513
ryanycw <ryan@ryanycw.dev>
2023-09-10 23:09:34 +0800

    build: update interface for liquidation related parameters

--- src/interfaces/submodules/IConfigureSubmodule.sol
+++ src/interfaces/submodules/IConfigureSubmodule.sol
@@ -11,8 +11,13 @@ interface IConfigureSubmodule {
     function configureExternalProtocol(address _nftPositionManager, address _masterchef) external;
     function configureInfrastructure(address _universalLiquidator, address _universalLiquidatorRegistry) external;
     function configurePool(address _token0, address _token1, uint24 _fee, string calldata _vaultName) external;
+    function addRewardTokens(address[] calldata _rewardTokens) external;
+    function removeRewardToken(uint256 _index) external;
+    function setUnifiedRewardToken(address _rewardToken) external;
+    function setUnifiedDepositToken(address _depositToken) external;
     function addPosition(uint256 _tokenId, uint256 _liquidity, int24 _tickLower, int24 _tickUpper) external;
     function updatePosition(uint256 _positionId, uint256 _tokenId, uint256 _liquidity, int24 _tickLower, int24 _tickUpper)
         external;
     function setVaultPause(bool _pause) external;
+    function setLiquidationRewardPause(bool _pause) external;
 }
