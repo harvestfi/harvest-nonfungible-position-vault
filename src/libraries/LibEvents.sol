4f8ebdc66478f44636b82934d02dbf4826b13f02
ryanycw <ryan@ryanycw.dev>
2023-09-10 23:11:15 +0800

    build: add new events for liquidation paramters configuration

--- src/libraries/LibEvents.sol
+++ src/libraries/LibEvents.sol
@@ -13,15 +13,20 @@ library LibEvents {
     event UpdateUpgradeExpiration(uint256 duration);
     event UpgradeCancelled(bytes32 id, address indexed who);
     // Configuration
-    event VaultPauseUpdate(bool paused);
     event FeeConfigurationUpdate(
         address indexed strategist, uint256 strategistFeeNumerator, uint256 platformFeeNumerator, uint256 profitSharingNumerator
     );
     event ExternalFarmingContractUpdate(address indexed nftPositionManager, address indexed masterchef);
     event InfrastructureUpdate(address indexed universalLiquidator, address indexed universalLiquidatorRegistry);
     event PoolConfigurationUpdate(address indexed token0, address indexed token1, uint24 fee, string name);
+    event RewardTokenAdded(uint256 fromIndex, uint256 toIndex);
+    event RewardTokenRemoved(address indexed removedToken);
+    event UnifiedRewardTokenUpdate(address indexed rewardToken);
+    event UnifiedDepositTokenUpdate(address indexed depositToken);
     event PositionAdd(uint256 positionId, uint256 tokenId, uint256 liquidity, int24 tickLower, int24 tickUpper);
     event PositionUpdate(uint256 positionId, uint256 tokenId, uint256 liquidity, int24 tickLower, int24 tickUpper);
+    event VaultPauseUpdate(bool paused);
+    event LiquidationRewardPauseUpdate(bool paused);
     // Initializable
     event Initialized(uint8 version);
     // TokenizedVault
