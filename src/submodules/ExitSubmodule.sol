d3f41a5d3b24a86fb969acc15f9b24add63fff30
ryanycw <ryan@ryanycw.dev>
2023-10-01 02:04:50 +0800

    build: init exit features

--- src/submodules/ExitSubmodule.sol
+++ src/submodules/ExitSubmodule.sol
@@ -7,4 +7,8 @@ import {IExitSubmodule} from "../interfaces/submodules/IExitSubmodule.sol";
 // Helpers
 import {Modifiers} from "../core/Modifiers.sol";
 
-contract ExitSubmodule is Modifiers, IExitSubmodule {}
+contract ExitSubmodule is Modifiers, IExitSubmodule {
+    function withdrawFullAmount() external {}
+
+    function withdrawByAmount() external {}
+}
