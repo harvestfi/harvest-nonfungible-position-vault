10ea4ac556511a20aee108802f5c7115489c664d
ryanycw <ryan@ryanycw.dev>
2023-10-18 22:24:40 +0800

    test: move swap to swap helper

--- test/unit-tests/T01Invest.t.sol
+++ test/unit-tests/T01Invest.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.17;
 
 // Utilities
 import {D01Deployment, console2} from "../utils/D01Deployment.t.sol";
+import {PoolHelper} from "../utils/PoolHelper.t.sol";
 
 // libraries
 import {Position} from "../../src/libraries/LibAppStorage.sol";
@@ -10,8 +11,6 @@ import {LibErrors} from "../../src/libraries/LibErrors.sol";
 
 // Interfaces
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../interfaces/IUSDT.sol";
-import "../../src/interfaces/protocols/pancake/IMasterChefV3.sol";
 
 contract Invest is D01Deployment {
     function setUp() public virtual override {
@@ -22,14 +21,13 @@ contract Invest is D01Deployment {
     }
 
     function testDoHardWork() public virtual {
-        vm.makePersistent(address(vault));
         addInvestSubmodule();
         // transfer tokens to the vault
-        changePrank(whale);
         uint256 _amount0 = IERC20(token0).balanceOf(whale);
         uint256 _amount1 = IERC20(token1).balanceOf(whale);
-        IERC20(token0).transfer(address(vault), _amount0);
-        IUSDT(token1).transfer(address(vault), _amount1);
+
+        deal(token0, address(vault), _amount0);
+        deal(token1, address(vault), _amount1);
 
         changePrank(governance);
         // configure external protocol
@@ -41,21 +39,19 @@ contract Invest is D01Deployment {
         _path[0] = 0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898;
         _path[1] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
         universalLiquidatorRegistry.setPath(bytes32(bytes("pancakeV3")), _path);
+        // USDC -> WETH
+        _path[0] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+        _path[1] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        universalLiquidatorRegistry.setPath(bytes32(bytes("pancakeV3")), _path);
         // USDT -> WETH
         _path[0] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
         _path[1] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
         universalLiquidatorRegistry.setPath(bytes32(bytes("pancakeV3")), _path);
-        // TUSD -> USDT -> WETH
-        _path = new address[](3);
-        _path[0] = 0x0000000000085d4780B73119b644AE5ecd22b376;
-        _path[1] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-        _path[2] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-        universalLiquidatorRegistry.setPath(bytes32(bytes("pancakeV3")), _path);
         // configure liquidation fee
         // CAKE -> WETH
         pancakeV3.setFee(0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 2500);
-        // TUSD -> USDT
-        pancakeV3.setFee(0x0000000000085d4780B73119b644AE5ecd22b376, 0xdAC17F958D2ee523a2206206994597C13D831ec7, 100);
+        // USDC -> WETH
+        pancakeV3.setFee(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 500);
         // USDT -> WETH
         pancakeV3.setFee(0xdAC17F958D2ee523a2206206994597C13D831ec7, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 500);
         // configure the pool
@@ -63,7 +59,7 @@ contract Invest is D01Deployment {
         // set reward token
         address[] memory _rewardTokens = new address[](3);
         _rewardTokens[0] = 0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898; // CAKE
-        _rewardTokens[1] = 0x0000000000085d4780B73119b644AE5ecd22b376; // TUSD
+        _rewardTokens[1] = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
         _rewardTokens[2] = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // USDT
         vault.addRewardTokens(_rewardTokens);
         // set unified reward token
@@ -75,11 +71,10 @@ contract Invest is D01Deployment {
         vault.createPosition(tickLower, tickUpper, amount0Desired, amount1Desired, amount0Min, amount1Min);
         // stake position
         vault.stakePosition(vault.positionCount() - 1);
-        uint256 tokenId = vault.positions(vault.positionCount() - 1).tokenId;
-        // simulate swap transaction that happened in the pool
-        bytes32 _hash = 0xc63ca44c870c098ef79c0c15c25006c8a586256dea53803bbb7f13e910626483;
-        vm.rollFork(forkNetwork, _hash);
-        tokenId = vault.positions(vault.positionCount() - 1).tokenId;
+
+        bytes memory encodedPath0 = abi.encodePacked(token0, fee, token1);
+        bytes memory encodedPath1 = abi.encodePacked(token1, fee, token0);
+        mockSwap(10, 0x1b81D678ffb9C0263b24A97847620C99d213eB14, encodedPath0, encodedPath1, token0, token1);
 
         // do hard work
         vault.doHardWork(vault.positionCount() - 1);
---
+++ test/utils/PoolHelper.t.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+// External Packages
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+// Utilities
+import {D00Defaults, console2} from "./D00Defaults.t.sol";
+
+// Interfaces
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../../src/interfaces/protocols/pancake/ISwapRouter.sol";
+import "../../src/interfaces/protocols/pancake/INonfungiblePositionManager.sol";
+import "../../src/interfaces/protocols/pancake/IPancakeV3Pool.sol";
+
+contract PoolHelper is D00Defaults {
+    using SafeERC20 for IERC20;
+
+    function mockSwap(uint256 _round, address _router, bytes memory _path0, bytes memory _path1, address _token0, address _token1)
+        public
+        virtual
+    {
+        address whale = makeAddr("Whale 0");
+        deal(_token0, whale, 10 ether);
+        for (uint256 index; index < _round;) {
+            console2.log(index);
+            changePrank(whale);
+
+            ISwapRouter.ExactInputParams memory swapInfo =
+                ISwapRouter.ExactInputParams(_path0, whale, block.timestamp, IERC20(_token0).balanceOf(whale), 1);
+            IERC20(_token0).safeApprove(_router, 0);
+            IERC20(_token0).safeApprove(_router, type(uint256).max);
+            ISwapRouter(_router).exactInput(swapInfo);
+
+            IERC20(_token1).safeApprove(_router, 0);
+            IERC20(_token1).safeApprove(_router, type(uint256).max);
+            swapInfo = ISwapRouter.ExactInputParams(_path1, whale, block.timestamp, IERC20(_token1).balanceOf(whale), 1);
+            ISwapRouter(_router).exactInput(swapInfo);
+
+            changePrank(governance);
+            unchecked {
+                index++;
+            }
+        }
+    }
+
+    function getPositionInfo(uint256 _tokenId) public virtual {
+        (
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            uint128 liquidity,
+            uint256 feeGrowthInside0LastX128,
+            uint256 feeGrowthInside1LastX128,
+            uint128 tokensOwed0,
+            uint128 tokensOwed1
+        ) = INonfungiblePositionManager(nonFungibleManagerPancake).positions(_tokenId);
+
+        console2.log("liquidity: ", liquidity);
+        console2.log("feeGrowthInside0LastX128: ", feeGrowthInside0LastX128);
+        console2.log("feeGrowthInside1LastX128: ", feeGrowthInside1LastX128);
+        console2.log("tokensOwed0: ", tokensOwed0);
+        console2.log("tokensOwed1: ", tokensOwed1);
+    }
+}
