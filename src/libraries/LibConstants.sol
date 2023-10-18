f5ba33e55b87d0fc49a5fcb0ec04a32525899d90
ryanycw <ryan@ryanycw.dev>
2023-10-18 20:56:04 +0800

    build: add constants for fee configuration

--- src/libraries/LibConstants.sol
+++ src/libraries/LibConstants.sol
@@ -11,4 +11,7 @@ library LibConstants {
     address internal constant _V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address internal constant _V2_FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
     address internal constant _UNI_STAKER = 0x1f98407aaB862CdDeF78Ed252D6f557aA5b0f00d;
+
+    uint256 internal constant _MAX_TOTAL_FEE = 3000;
+    uint256 internal constant _FEE_DENOMINATOR = 10000;
 }
