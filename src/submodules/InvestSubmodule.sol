f9476fdb4aaa83353c2935421587d3cbd6a60c52
ryanycw <ryan@ryanycw.dev>
2023-10-18 22:24:20 +0800

    chore: adjust import and logs

--- src/submodules/InvestSubmodule.sol
+++ src/submodules/InvestSubmodule.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.17;
 import "forge-std/Test.sol";
 
 // External Packages
-import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 // Interfaces
 import {IInvestSubmodule} from "../interfaces/submodules/IInvestSubmodule.sol";
@@ -24,13 +24,13 @@ contract InvestSubmodule is Modifiers, IInvestSubmodule {
 
     function doHardWork(uint256 _positionId) external override onlyGovernanceOrController {
         console2.log("Before Claim (CAKE): ", IERC20(0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898).balanceOf(address(this)));
-        console2.log("Before Claim (TUSD): ", IERC20(0x0000000000085d4780B73119b644AE5ecd22b376).balanceOf(address(this)));
+        console2.log("Before Claim (USDC): ", IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48).balanceOf(address(this)));
         console2.log("Before Claim (USDT): ", IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7).balanceOf(address(this)));
         // claim rewards: harvest + collect
         LibPositionManager.harvest(_positionId);
         LibPositionManager.collect(_positionId);
         console2.log("After Claim (CAKE): ", IERC20(0x152649eA73beAb28c5b49B26eb48f7EAD6d4c898).balanceOf(address(this)));
-        console2.log("After Claim (TUSD): ", IERC20(0x0000000000085d4780B73119b644AE5ecd22b376).balanceOf(address(this)));
+        console2.log("After Claim (USDC): ", IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48).balanceOf(address(this)));
         console2.log("After Claim (USDT): ", IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7).balanceOf(address(this)));
         // liquidate to universal reward token through universal liquidators
         if (s.liquidationRewardPause) {
