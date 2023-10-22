b431c5beb79e3f3679a27a0a8575163ef2140041
ryanycw <ryan@ryanycw.dev>
2023-10-23 00:38:31 +0800

    build: add new error for new fee configuration

--- src/libraries/LibErrors.sol
+++ src/libraries/LibErrors.sol
@@ -2,6 +2,11 @@
 pragma solidity 0.8.17;
 
 library LibErrors {
+    enum TimestampErrorCodes {
+        CannotBeZero,
+        TooEarly
+    }
+
     // Library Checkers
     error AddressUnconfigured();
     // Access Control List
@@ -9,8 +14,12 @@ library LibErrors {
     error NotGovernance();
     error NotGovernanceOrController();
     // Configuration
+    error FeeTooHigh(uint256 _maxFee, uint256 _setFee);
+    error FeeParametersUnconfigured(uint256 _numerator, uint256 _timestamp);
+    error FeeUpdateExpired(uint256 _nextFeeTimestamp);
     error TokenAlreadyRegistered(address _token);
     error InvalidToken(address _token);
+    error InvalidTimestamp(TimestampErrorCodes _errorCode, uint256 _timestamp);
     error DecimalsMismatch(address _token0, uint8 _decimals0, address _token1, uint8 _decimals1);
     // Position
     error PositionStaked(uint256 _positionId);
