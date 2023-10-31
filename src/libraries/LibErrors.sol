4d36fa31af19136064deb5659f2f1e3f4f00115e
ryanycw <ryan@ryanycw.dev>
2023-10-31 16:05:07 +0800

    chore: remove unused errors

--- src/libraries/LibErrors.sol
+++ src/libraries/LibErrors.sol
@@ -12,15 +12,10 @@ library LibErrors {
     // Library Checkers
     error AddressUnconfigured();
     // Access Control List
-    error NoPermission();
     error NotGovernance();
     error NotGovernanceOrController();
     // Configuration
-    error FeeTooHigh(uint256 _maxFee, uint256 _setFee);
-    error FeeParametersUnconfigured(uint256 _numerator, uint256 _timestamp);
-    error FeeUpdateExpired(uint256 _nextFeeTimestamp);
     error TokenAlreadyRegistered(address _token);
-    error InvalidToken(address _token);
     error InvalidTimestamp(TimestampErrorCodes _errorCode, uint256 _timestamp);
     error InvalidAddress(AddressErrorCodes _errorCode, address _address);
     error DecimalsMismatch(address _token0, uint8 _decimals0, address _token1, uint8 _decimals1);
