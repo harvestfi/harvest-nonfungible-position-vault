88f960e073e149e104c741e0ac6d227ca186cc56
ryanycw <ryan@ryanycw.dev>
2024-01-15 10:37:07 +0900

    build: init test structure

--- test/unit-tests/T01TokenizedVault.t.sol
+++ test/unit-tests/T01TokenizedVault.t.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
+// Utilities
+import {D01Deployment, console2} from "../utils/D01Deployment.t.sol";
+
+contract T01TokenizedVault is D01Deployment {
+    function setUp() public virtual override {
+        // TODO
+    }
+
+    function testTokenizedVault() public virtual {
+        // TODO
+    }
+}
